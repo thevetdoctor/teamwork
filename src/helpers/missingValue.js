f38281c5c9ea0a5629a4f6797de27f0bc1678e7e
obafemi oderanti <thevetdoctor.gmail.com>
2020-02-26 03:18:19 +0100

    feature(response):include isAdmin in response
    signin response
    
    [Finishes #30076431]

--- src/helpers/missingValue.js
+++ src/helpers/missingValue.js
@@ -4,6 +4,6 @@ exports.values = (res, ...args) => {
     const argumentsObj = { args };
 
     const missingValue = Object.keys(argumentsObj).filter(item => ((argumentsObj[item] === undefined) || (argumentsObj[item] === '')));
-    // console.log('missing value(s)', missingValue, args);
+    console.log('missing value(s)', missingValue, args);
     if (missingValue.length < 0) return response.values(res, 400, `${missingValue} not supplied`);
 }
